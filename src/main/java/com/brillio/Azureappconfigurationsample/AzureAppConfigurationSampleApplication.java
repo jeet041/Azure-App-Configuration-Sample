7d2ab0280c1bb755b17d290d11db2eb1c80e5ddc
jeet041 <mahorjitendra@gmail.com>
2023-08-07 17:10:15 +0530

    Update AzureAppConfigurationSampleApplication.java

--- src/main/java/com/brillio/Azureappconfigurationsample/AzureAppConfigurationSampleApplication.java
+++ src/main/java/com/brillio/Azureappconfigurationsample/AzureAppConfigurationSampleApplication.java
@@ -4,8 +4,10 @@ import com.brillio.Azureappconfigurationsample.configuration.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
 @EnableConfigurationProperties(AppProperties.class)
 public class AzureAppConfigurationSampleApplication {
 
