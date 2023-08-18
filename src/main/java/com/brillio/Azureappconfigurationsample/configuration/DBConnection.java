e5200ba8afbee87b02daa388ea44c061f1cc1fe2
jeet041 <mahorjitendra@gmail.com>
2023-08-18 18:53:19 +0530

    Update DBConnection.java

--- src/main/java/com/brillio/Azureappconfigurationsample/configuration/DBConnection.java
+++ src/main/java/com/brillio/Azureappconfigurationsample/configuration/DBConnection.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@ConfigurationProperties
+@Component
 @RefreshScope
 public class DBConnection {
 
