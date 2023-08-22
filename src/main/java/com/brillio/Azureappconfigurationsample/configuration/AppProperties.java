5a6e1270c8d2346b5c35cf52a7223a17a589b29f
jeet041 <mahorjitendra@gmail.com>
2023-08-22 19:03:39 +0530

    added configuration

--- src/main/java/com/brillio/Azureappconfigurationsample/configuration/AppProperties.java
+++ src/main/java/com/brillio/Azureappconfigurationsample/configuration/AppProperties.java
@@ -2,7 +2,7 @@ package com.brillio.Azureappconfigurationsample.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
+@Configuration
 @ConfigurationProperties
 public class AppProperties {
     public String getConfigMsg() {
--- src/main/java/com/brillio/Azureappconfigurationsample/configuration/ConfigProperties.java
+++ src/main/java/com/brillio/Azureappconfigurationsample/configuration/ConfigProperties.java
@@ -1,7 +1,9 @@
 package com.brillio.Azureappconfigurationsample.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "app")
 public class ConfigProperties {
     public String getConfigMsg() {
